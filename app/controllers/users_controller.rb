bbda2ec2b24af9f57bbd129c8591f19ecf9230a0
chintusai <chintusai.maddi@prasthana.com>
2022-04-27 18:38:09 +0530

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,29 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user= User.find(params[:id])
+    if @user.update(params.require(:user).permit(:username,:email,:password))
+      flash[:notice]='user updated'
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @user=User.find(params[:id])
+    @articles= Article.where(user_id:@user.id)
+    @articles.each do |article|
+      article.destroy
+    end
     @user.destroy
+    flash[:notice]='user is destroyed'
     redirect_to users_path
   end
 
